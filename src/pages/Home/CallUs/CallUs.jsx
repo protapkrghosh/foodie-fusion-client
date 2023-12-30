bf9448f6b23ba8691a3590394e62e29b5e5c9166
Protap Ghosh <protapg518@gmail.com>
2023-12-30 22:42:42 +0600

    Added call us section

---
+++ src/pages/Home/CallUs/CallUs.jsx
@@ -0,0 +1,9 @@
+const CallUs = () => {
+  return (
+    <section className="h-44 bg-[#1E1E1E] text-3xl md:text-4xl font-semibold text-white flex items-center justify-center">
+      <h3>Call Us: +880 1923934809</h3>
+    </section>
+  );
+};
+
+export default CallUs;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Category from "../Category/Category";
 import ChefService from "../../../components/ChefService/ChefService";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import bgImage from "../../../assets/others/Rectangle9.png"
+import CallUs from "../CallUs/CallUs";
 
 const Home = () => {
   const bgImageStyle = {
@@ -19,6 +20,7 @@ const Home = () => {
         content={'Consectetur adipisicing elit. Doloremque assumenda reprehenderit illum voluptatibus rem facere. Ex fugiat nam sit at hic dolores recusandae consequuntur deserunt, harum veritatis aspernatur quibusdam temporibus.'}
       />
       <PopularMenu />
+      <CallUs />
     </div>
   );
 };
