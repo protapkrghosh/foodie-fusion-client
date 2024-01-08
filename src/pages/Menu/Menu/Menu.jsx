3d42c866f548670ff46c070d0ad9a9ac5d45d2a7
Protap Ghosh <protapg518@gmail.com>
2024-01-08 13:13:08 +0600

    Create salad and soup item sections

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,10 @@
 import { Helmet } from 'react-helmet-async';
-import Cover from '../../Shared/Cover/Cover';
-import menuImg from '../../../assets/menu/banner3.jpg'
 import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
 import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
 import saladImg from '../../../assets/menu/salad-bg.jpg'
 import soupImg from '../../../assets/menu/soup-bg.jpg'
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
 import { useMenu } from '../../../hooks/useMenu';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import MenuCategory from '../MenuCategory/MenuCategory';
@@ -35,7 +35,7 @@ const Menu = () => {
         sectionBg={dessertImg}
         items={desserts}
         title='Dessert'
-        content='THE INDUSTRY’S STANDARD DUMMY TEXT EVER SINCE THE 1500S, WHEN AN UNKNOWN PRINTER TOOK A GALLEY OF TYPE AND SCRAMBLED IT TO MAKE A TYPE SPECIMEN BOOK.'
+        content='THE INDUSTRY’S STANDARD DUMMY TEXT EVER SINCE THE 1500S, WHEN AN UNKNOWN PRINTER TOOK A GALLEY OF TYPE AND SCRAMBLED IT TO MAKE A TYPE SPECIMEN BOOK'
       />
 
       {/* Pizza menu items */}
@@ -44,6 +44,22 @@ const Menu = () => {
         items={pizza}
         title='Pizza'
         content='Officia beatae odit accusamus reiciendis quo voluptatum sunt distinctio dolorum adipisci quasi? Ipsum ipsa minus labore! Sapiente sit, repudiandae dolores'
+      />
+
+      {/* Salad menu items */}
+      <MenuCategory
+        sectionBg={saladImg}
+        items={salad}
+        title='Salad'
+        content='Quia accusamus porro voluptate pariatur optio fugiat ex odit dolores velit nobis impedit quod voluptatum facere voluptates modi'
+      />
+
+      {/* Soup menu items */}
+      <MenuCategory
+        sectionBg={soupImg}
+        items={soup}
+        title='Soup'
+        content='Reprehenderit autem consectetur expedita ea culpa rerum provident assumenda tenetur, iste accusantium modi. Sint debitis tenetur illo officia '
       />
     </div>
   );
