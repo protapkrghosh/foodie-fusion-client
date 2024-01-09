35f5c3ff505c298e9a90da931bd2e9b4b556d688
Protap Ghosh <protapg518@gmail.com>
2024-01-09 21:50:03 +0600

    login form style

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,10 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../providers/AuthProvider';
+import { Link } from 'react-router-dom'
 import img from '../../assets/others/authentication2.png'
 import loginBg from '../../assets/others/authentication.png'
+import { FaFacebookF, FaGoogle, FaGithub } from "react-icons/fa6";
 
 const Login = () => {
   const captchaRef = useRef(null);
@@ -36,15 +38,15 @@ const Login = () => {
   }
 
   return (
-    <>
-      <div className="hero min-h-screen" style={{ backgroundImage: `url(${loginBg})` }}>
-        <div className="hero-content flex-col lg:flex-row shadowBg pt-5">
-          <div className="text-center lg:text-left w-1/2">
-            <img src={img} alt="Image" className='md:w-[700px]' />
+    <div className="hero min-h-screen" style={{ backgroundImage: `url(${loginBg})` }}>
+      <div className='my-14'>
+        <div className="hero-content flex-col lg:flex-row shadowBg py-8">
+          <div className="text-center lg:text-left md:w-1/2">
+            <img src={img} alt="Image" className='w-[500px] lg:w-[600px]' />
           </div>
 
           {/* Login Form */}
-          <div className="card w-1/2 max-w-md">
+          <div className="card w-full lg:w-1/2 md:max-w-sm">
             <h3 className='text-3xl text-center font-bold -mb-5'>Login</h3>
             <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
@@ -78,10 +80,28 @@ const Login = () => {
                 <input disabled={disabled} type="submit" value="Sign In" className="text-white text-[16px] btn bg-[#D1A054] hover:bg-[#bd904c] uppercase" />
               </div>
             </form>
+
+            {/* Social Media */}
+            <div className='flex flex-col justify-center items-center -mt-3'>
+              <p className='text-[#D1A054]'><small>New here? <Link to='/signUp' className='font-semibold hover:underline'>Create a New Account</Link> </small></p>
+              <p className='text-[15px] my-2 font-semibold'>Or sign in with</p>
+
+              <div className='flex justify-center items-center space-x-8'>
+                <span className='border border-black p-[9px] rounded-full text-[20px] cursor-pointer group'>
+                  <FaFacebookF className='group-hover:scale-125 duration-200' />
+                </span>
+                <span className='border border-black p-[9px] rounded-full text-[20px] cursor-pointer group'>
+                  <FaGoogle className='group-hover:scale-125 duration-200' />
+                </span>
+                <span className='border border-black p-[9px] rounded-full text-[20px] cursor-pointer group'>
+                  <FaGithub className='group-hover:scale-125 duration-200' />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
