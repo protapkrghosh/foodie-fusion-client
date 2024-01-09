f00189cbec2661aeb9fb3139267b18c59cf0d75e
Protap Ghosh <protapg518@gmail.com>
2024-01-09 13:41:10 +0600

    Added login form in project

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname.includes('login');
+
   return (
     <div>
-      <NavBar />
+      { hideHeaderFooter || <NavBar />}
       <Outlet />
-      <Footer />
+      {hideHeaderFooter || <Footer />}
     </div>
   );
 };
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -5,6 +5,7 @@ const NavBar = () => {
     <li><Link to="/">Home</Link></li>
     <li><Link to="/menu">Our Menu</Link></li>
     <li><Link to="/order/salad">Our Shop</Link></li>
+    <li><Link to="/login">Login</Link></li>
   </>
 
   return (
