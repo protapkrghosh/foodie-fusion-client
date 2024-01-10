b81d043a67d3f0b1aea4e228553af38ab7ad9114
Protap Ghosh <protapg518@gmail.com>
2024-01-10 15:39:37 +0600

    update section title

--- src/components/ChefService/ChefService.jsx
+++ src/components/ChefService/ChefService.jsx
@@ -18,7 +18,7 @@ const ChefService = ({ title, content, sectionBg }) => {
     >
       <div className="text-center h-[500px] bg-cover bg-center flex justify-center items-center">
         <div className="bg-white bg-opacity-95 w-[80%] md:w-[60%] p-8 md:p-10 lg:p-12 rounded-sm">
-          <h2 className="text-3xl text-[#4D4C49] font-semibold uppercase mb-5 border-t-[1px] border-b-[1px] border-[#252422] py-2 w-full md:w-[21%] mx-auto">{title}</h2>
+          <h2 className="text-3xl text-[#4D4C49] font-semibold uppercase mb-5 border-t-[1px] border-b-[1px] border-[#252422] py-2 w-[50%] md:w-[21%] mx-auto">{title}</h2>
           <p className="text-[#5F5F5C] text-[15px] leading-7 uppercase">{content}</p>
         </div>
       </div>
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className="text-center md:w-3/12 mx-auto my-8">
+    <div className="text-center w-5/12 md:w-3/12 mx-auto my-8">
       <p className="text-[#D99904] mb-2 italic">--- {subHeading} ---</p>
       <h3 className="text-3xl border-y-2 py-3 uppercase">{heading}</h3>
     </div>
