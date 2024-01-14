10d1191e98a21883da5b12bac8a586f7508b5ba8
Protap Ghosh <protapg518@gmail.com>
2024-01-14 21:33:05 +0600

    added axios

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,36 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../providers/AuthProvider"
+import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+
+export const useAxiosSecure = () => {
+  const { logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const axiosSecure = axios.create({
+    baseURL: "http://localhost:5000"
+  });
+
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+          await logOut();
+          navigate('signin');
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate, axiosSecure]);
+
+  return [axiosSecure];
+}
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,20 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react'
 import { AuthContext } from '../providers/AuthProvider'
+import { useAxiosSecure } from './useAxiosSecure';
 
 export const useCart = () => {
   const { user } = useContext(AuthContext);
   const token = localStorage.getItem('access_token');
+  const [axiosSecure] = useAxiosSecure();
 
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ['carts', user?.email],
+    // General use case
+    // queryFn: async () => {
+    //   const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+    //     headers: {
+    //       authorization: `bearer ${token}`
+    //     }
+    //   })
+    //   return res.json();
+    // },
+
+    // Interceptor use case
     queryFn: async () => {
-      const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
-        headers: {
-          authorization: `bearer ${token}`
-        }
-      })
-      return res.json();
+      const res = await axiosSecure(`/carts?email=${user?.email}`)
+      console.log(res);
+      return res.data;
     },
   })
   return [cart, refetch]
