1220d4b394852a87aaa36a73331c84159850faef
Protap Ghosh <protapg518@gmail.com>
2024-01-27 01:14:50 +0600

    upload image form

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -4,9 +4,24 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { ImSpoonKnife } from "react-icons/im";
 
+const img_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
 const AddItem = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
+
+  const onSubmit = data => {
+    const formData = new FormData();
+    formData.append('image', data.image[0])
+
+    fetch(img_hosting_url, {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(imgResponse => {
+        console.log(imgResponse);
+      })
+  };
   console.log(errors);
 
   return (
@@ -24,7 +39,8 @@ const AddItem = () => {
               <div className="label">
                 <p className="label-text text-[15px] font-semibold">Recipe name<span className='text-rose-600'>*</span></p>
               </div>
-              <input type="text" placeholder="Recipe name" className="input" />
+              <input type="text" placeholder="Recipe name" name="name" {...register("name", { required: true, maxLength: 120 })} className="input" />
+              {errors.name && <span className='text-[14px] text-rose-600'>Name is required</span>}
             </label>
 
             <div className="flex gap-5">
@@ -32,21 +48,23 @@ const AddItem = () => {
                 <div className="label">
                   <p className="label-text text-[15px] font-semibold">Category<span className='text-rose-600'>*</span></p>
                 </div>
-                <select className="select">
-                  <option disabled selected>Pick one</option>
+                <select defaultValue="Pick One" className="select" name="category" {...register("category", { required: true })}>
+                  <option disabled>Pick One</option>
                   <option>Salad</option>
                   <option>Pizza</option>
                   <option>Soup</option>
                   <option>Dessert</option>
                   <option>Drink</option>
                 </select>
+                {errors.category && <span className='text-[14px] text-rose-600'> Category is required</span>}
               </label>
 
               <label className="form-control w-full">
                 <div className="label">
                   <p className="label-text text-[15px] font-semibold">Price<span className='text-rose-600'>*</span></p>
                 </div>
-                <input type="number" placeholder="Price" className="input" />
+                <input type="number" placeholder="Price" name="price" {...register("price", { required: true })} className="input" />
+                {errors.price && <span className='text-[14px] text-rose-600'> Price is required</span>}
               </label>
             </div>
 
@@ -54,15 +72,19 @@ const AddItem = () => {
               <div className="label">
                 <p className="label-text text-[15px] font-semibold">Recipe Details<span className='text-rose-600'>*</span></p>
               </div>
-              <textarea className="textarea h-40" placeholder="Recipe Details"></textarea>
+              <textarea className="textarea h-40" placeholder="Recipe Details" name="details" {...register("recipe", { required: true })}></textarea>
+              {errors.recipe && <span className='text-[14px] text-rose-600'> Details is required</span>}
             </label>
 
             <div className="pb-2">
-              <input type="file" className="file:text-[15px] file:font-semibold file:border-dashed file:border-[1px] file:border-slate-400 file:px-4 file:py-1 w-full max-w-xs" />
+              <div>
+                <input type="file" name="image" {...register("image", { required: true })} className="file:text-[15px] file:bg-zinc-300 file:font-semibold file:border-dashed file:border-[1px] file:border-slate-400 file:px-4 file:py-1 w-full max-w-xs" />
+              </div>
+              {errors.image && <span className='text-[14px] text-rose-600'> Recipe image is required</span>}
             </div>
 
             <div className="flex items-center h-10 w-[130px] relative bg-gradient-to-r from-[#835D23] to-[#B58130] px-5 cursor-pointer text-white rounded-sm">
-              <input type="button" value="Add Item" className="cursor-pointer"/>
+              <input type="submit" value="Add Item" className="cursor-pointer" />
               <span className="absolute right-5"><ImSpoonKnife /></span>
             </div>
           </form>
