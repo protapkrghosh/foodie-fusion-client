a9e4db6897234b415ca7465e89349b45ef547ad0
Protap Ghosh <protapg518@gmail.com>
2024-01-27 16:47:03 +0600

    manage recipe items for admin

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -8,7 +8,7 @@ export const useMenu = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      fetch('http://localhost:5000/menu')
+      fetch('https://foodiefusionserver.vercel.app/menu')
         .then(res => res.json())
         .then(data => {
           setMenu(data);
@@ -18,12 +18,12 @@ export const useMenu = () => {
   */
 
   // Use by react query
-  const { data: menu = [], isLoading: loading } = useQuery({
+  const { data: menu = [], isLoading: loading, refetch } = useQuery({
     queryKey: ['menu'],
     queryFn: async () => {
-      const res = await fetch('http://localhost:5000/menu');
+      const res = await fetch('https://foodiefusionserver.vercel.app/menu');
       return res.json();
     }
   })
-  return [menu, loading];
+  return [menu, loading, refetch];
 }
--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,11 +1,37 @@
+import Swal from "sweetalert2";
 import { Helmet } from "react-helmet-async";
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBinLine } from "react-icons/ri";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useMenu } from "../../../hooks/useMenu";
+import { useAxiosSecure } from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const ManageItems = () => {
-  const [menu] = useMenu();
+  const [menu, loading, refetch] = useMenu();
+  const [axiosSecure] = useAxiosSecure();
+
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You won't be able to revert ${item.name}`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/menu/${item._id}`)
+          .then(res => {
+            if (res.data.deletedCount > 0) {
+              refetch();
+              toast.success(`${item.name} deleted successfully`);
+            }
+          })
+      }
+    });
+  }
 
   return (
     <div className="w-full mx-auto bg-[#f6f6f6]">
@@ -50,14 +76,14 @@ const ManageItems = () => {
                       <p className="text-[16px] font-semibold">{item.name}</p>
                     </td>
                     <td>
-                      <p className="text-[#999999] italic capitalize mt-[2px]">{item.category}</p>
+                      <p className="text-[#a5a5a5] capitalize mt-[2px]">{item.category}</p>
                     </td>
                     <td className="font-semibold">${item.price}</td>
                     <td>
                       <button className="text-white text-[18px] flex justify-center items-center w-8 h-8 bg-[#D1A054] hover:bg-[#B98D4A] rounded-md duration-200"><FaRegEdit /></button>
                     </td>
                     <td>
-                      <button className="text-white text-xl flex justify-center items-center w-8 h-8 bg-rose-600 hover:bg-rose-700 rounded-md duration-200"><RiDeleteBinLine /></button>
+                      <button onClick={() => handleDelete(item)} className="text-white text-xl flex justify-center items-center w-8 h-8 bg-rose-600 hover:bg-rose-700 rounded-md duration-200"><RiDeleteBinLine /></button>
                     </td>
                   </tr>)
                 }
